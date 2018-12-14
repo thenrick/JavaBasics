b5023fd883a34b6de426bd61db9481e4cd2fd984
Tom Henricksen <tom.henricksen@gmail.com>
2018-12-13 19:01:02 -0500

    Create Basketball.java

---
+++ JavaVariables/src/Basketball.java
@@ -0,0 +1,9 @@
+
+public class Basketball {
+	private String type;
+	private static int count;
+	private static final int maxCount = 100;
+	private float diameter;
+	
+	static final int PLAYER_MULTIPLIER = 7;
+}
